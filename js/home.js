10fea23df1d59f4595f1b7028e407de7e4df146b
samidzaman <saqmidzaman@gmail.com>
2025-03-19 00:46:44 +0600

    pertial work added

--- js/home.js
+++ js/home.js
@@ -1,1 +1,15 @@
-console.log('home added');
+//add mony to the amount
+
+//step 1 add event handler in add mony button
+document.getElementById('add-mony-btn').addEventListener('click', function (event) {
+    // event by defualt for reloading form
+    event.preventDefault();
+
+    //step -2 get the input value of amount 
+    const addAmount = document.getElementById('input-amount').value;
+    console.log(addAmount);
+    
+      //step -3 get the input value of pin 
+      const pinInput = document.getElementById('input-pin').value;
+      console.log(pinInput);
+});
